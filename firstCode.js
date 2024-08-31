eb47b81a25bf3ff61c7016a8eb98448f1359809b
Rockey C.R <rockeyrai234@gmail.com>
2024-08-31 08:46:12 +0545

    sum function added

--- firstCode.js
+++ firstCode.js
@@ -2,3 +2,8 @@ const firstVar = " this is for the practice fo the git and github"
 
 const numberVar =[42,28,3]
 
+// this is my 9th try to change this file/code
+
+function numberSum(a,b){
+  return a + b
+}
